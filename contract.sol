79adbcabcc660199c54eada8ad072cc1d8fff693
SUBHAM KUNWAR <68282784+rabbit-sol@users.noreply.github.com>
2022-11-22 17:30:51 +0530

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -1,88 +1,91 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
 
-import 'erc721a/contracts/extensions/ERC721AQueryable.sol';
-import '@openzeppelin/contracts/access/Ownable.sol';
-import '@openzeppelin/contracts/utils/cryptography/MerkleProof.sol';
-import '@openzeppelin/contracts/security/ReentrancyGuard.sol';
-
-
-pragma solidity ^0.8.0;
-
-library Strings {
-    bytes16 private constant _HEX_SYMBOLS = "0123456789abcdef";
-
-    /**
-     * @dev Converts a `uint256` to its ASCII `string` decimal representation.
-     */
-    function toString(uint256 value) internal pure returns (string memory) {
-        
-
-        if (value == 0) {
-            return "0";
-        }
-        uint256 temp = value;
-        uint256 digits;
-        while (temp != 0) {
-            digits++;
-            temp /= 10;
-        }
-        bytes memory buffer = new bytes(digits);
-        while (value != 0) {
-            digits -= 1;
-            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
-            value /= 10;
-        }
-        return string(buffer);
-    }
-}
-
-
-contract YourNftToken is ERC721AQueryable, Ownable, ReentrancyGuard {
-
-   using Strings for uint256;
-
-  bytes32 public merkleRoot;
-  mapping(address => bool) public whitelistClaimed;
-
-  string baseURI;
-  string public uriSuffix = '.json';
-  string public hiddenMetadataUri;
-  
-  uint256 public cost;
-  uint256 public maxSupply;
-  uint256 public maxMintAmountPerTx;
-
-  bool public paused = false;
-  bool public whitelistMintEnabled = true;
-  bool public revealed = false;
-  mapping(address => bool) public whitelist;
-
-
-    string _tokenName="Ghost";
-    string _tokenSymbol="GH";
-    uint256 public _cost = 0 ether;
-    string _hiddenMetadataUri="ipfs://QmS2KcMpUzovsbd1xA7t6tQ5GZv9AB1ag7Ut8SAM2DDNeG/hidden.json";
-
-  constructor(uint256 _maxSupply,uint256 _maxMintAmountPerTx) ERC721A(_tokenName, _tokenSymbol) {
-    setCost(_cost);
-    maxSupply = _maxSupply;
-    setMaxMintAmountPerTx(_maxMintAmountPerTx);
-    setHiddenMetadataUri(_hiddenMetadataUri);
-  }
-
-  modifier mintCompliance(uint256 _mintAmount) {
-    require(_mintAmount > 0 && _mintAmount <= maxMintAmountPerTx, 'Invalid mint amount!');
-    require(totalSupply() + _mintAmount <= maxSupply, 'Max supply exceeded!');
-    _;
-  }
-
-  modifier mintPriceCompliance(uint256 _mintAmount) {
-    require(msg.value >= cost * _mintAmount, 'Insufficient funds!');
-    _;
-  }
+/*
  
+
+*/
+
+
+import 'operator-filter-registry/src/DefaultOperatorFilterer.sol';
+import '@openzeppelin/contracts/security/ReentrancyGuard.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+import '@openzeppelin/contracts/utils/Strings.sol';
+import 'erc721a/contracts/ERC721A.sol';
+
+
+
+pragma solidity >=0.8.17 <0.9.0;
+
+contract SampleERC721a is ERC721A, Ownable, ReentrancyGuard, DefaultOperatorFilterer {
+
+  using Strings for uint256;
+
+// ================== Variables Start =======================
+
+  
+  // reveal uri - set it in contructor
+  string internal uri;
+  string public uriSuffix = ".json";
+
+  // hidden uri - replace it with yours
+  string public hiddenMetadataUri = "ipfs://QmS2KcMpUzovsbd1xA7t6tQ5GZv9AB1ag7Ut8SAM2DDNeG/hidden.json";
+
+  // prices - replace it with yours
+  uint256 public price = 0 ether;
+
+  // supply - replace it with yours
+  uint256 public maxSupply = 10000;
+
+  // max per tx - replace it with yours
+  uint256 public maxMintAmountPerTx = 2;
+
+  // max per wallet - replace it with yours
+  uint256 public maxLimitPerWallet = 3;
+
+  // enabled
+  bool public publicSale=true;
+
+  // reveal
+  bool public revealed =false;
+
+  // mapping to keep track
+  mapping(address => uint256) public publicMintCount;
+
+  // total mint trackers
+  uint256 public publicMinted;
+
+    //whitelist
+     mapping(address => bool) public whitelist;
+     bool public whitelistEnable = false;
+     //unlimited mints
+     mapping(address=>bool) allowedUnlimitedMint;
+     //royalties
+
+     address public royaltyReceiver;
+     uint96 public royaltyFeesInBeeps;
+     bytes4 private constant _INTERFACE_ID_ERC2981 = 0x2a55205a;
+
+
+
+
+// ================== Variables End =======================  
+
+// ================== Constructor Start =======================
+
+  // Token NAME and SYMBOL - Replace it with yours
+  constructor(
+    string memory _uri, address _royalityAddress
+  ) ERC721A("GHOSTLERSRoyalty", "GHOST")  {
+    seturi(_uri);
+    royaltyFeesInBeeps=1000;
+    royaltyReceiver=_royalityAddress;
+    
+  }
+
+// ================== Constructor End =======================
+
+// add whitelist
 
     /**
      * @notice Add to whitelist
@@ -106,83 +109,233 @@ contract YourNftToken is ERC721AQueryable, Ownable, ReentrancyGuard {
         }
     }
 
+    /**
+     * @notice Function with whitelist
+     */
+//set mint limit
+function setUnlimitedMintPerAddress(address _address) public  onlyOwner{
+    allowedUnlimitedMint[_address] = true; 
+}
 
+// ================== Mint Functions Start =======================
 
+  function PublicMint(uint256 _mintAmount) public payable {
+    
+    // Normal requirements 
+    require(publicSale==true, 'The PublicSale is paused!');
+    require(_mintAmount > 0 && _mintAmount <= maxMintAmountPerTx, 'Invalid mint amount!');
+    require(totalSupply() + _mintAmount <= maxSupply, 'Max supply exceeded!');
+   
+    require(msg.value >= price * _mintAmount, 'Insufficient funds!');
+     
+    // Mint
+    if(allowedUnlimitedMint[msg.sender]==true){
+       
+        if(whitelistEnable==true){
+            require(whitelist[msg.sender], "NOT_IN_WHITELISTs");        
+        }
+            _safeMint(_msgSender(), _mintAmount);     
+    }
 
-  function mint(uint256 _mintAmount) public payable mintCompliance(_mintAmount) mintPriceCompliance(_mintAmount) {
-    require(!paused, 'The contract is paused!');
+    else{
+       require(publicMintCount[msg.sender] + _mintAmount <= maxLimitPerWallet, 'Max mint per wallet exceeded!');
+        if(whitelistEnable==true){
+        require(whitelist[msg.sender], "NOT_IN_WHITELIST");        
+      }        
+        _safeMint(_msgSender(), _mintAmount);
+    }
+    // Mapping update 
+    publicMintCount[msg.sender] += _mintAmount;  
+    publicMinted += _mintAmount;   
+  }  
 
-    _safeMint(_msgSender(), _mintAmount);
-
-  }
-  
-  function mintForAddress(uint256 _mintAmount, address _receiver) public mintCompliance(_mintAmount) onlyOwner {
+  function OwnerMint(uint256 _mintAmount, address _receiver) public onlyOwner {
+    require(totalSupply() + _mintAmount <= maxSupply, 'Max supply exceeded!');
     _safeMint(_receiver, _mintAmount);
   }
 
-  function _startTokenId() internal view virtual override returns (uint256) {
-    return 1;
+
+  
+
+// ================== Mint Functions End =======================  
+
+// ================== Set Functions Start =======================
+
+
+// reveal
+  function setRevealed(bool _state) public onlyOwner {
+    revealed = _state;
   }
 
-   function tokenURI(uint256 _tokenId) public view virtual override returns (string memory)
-  {
-    require(_exists(_tokenId),"ERC721Metadata: URI query for nonexistent token" );    
-    if(revealed == false) {
-        return hiddenMetadataUri;
-    }
-    string memory currentBaseURI = _baseURI();
-    return bytes(currentBaseURI).length > 0
-        ? string(abi.encodePacked(currentBaseURI, _tokenId.toString(), uriSuffix))
-        : "";
-  }
-
-  function reveal() public onlyOwner {
-    revealed = true;
-  }
-
-  function setCost(uint256 new_cost) public onlyOwner {
-    cost = new_cost;
-  }
-
-  function setMaxMintAmountPerTx(uint256 _maxMintAmountPerTx) public onlyOwner {
-    maxMintAmountPerTx = _maxMintAmountPerTx;
-  }
-
-  function setHiddenMetadataUri(string memory new_hiddenMetadataUri) public onlyOwner {
-    hiddenMetadataUri = new_hiddenMetadataUri;
-  }
-
-  function setBaseURI(string memory _newBaseURI) public onlyOwner {
-    baseURI = _newBaseURI;
+// uri
+  function seturi(string memory _uri) public onlyOwner {
+    uri = _uri;
   }
 
   function setUriSuffix(string memory _uriSuffix) public onlyOwner {
     uriSuffix = _uriSuffix;
   }
 
-  function setPaused(bool _state) public onlyOwner {
-    paused = _state;
+  function setHiddenMetadataUri(string memory _hiddenMetadataUri) public onlyOwner {
+    hiddenMetadataUri = _hiddenMetadataUri;
+  }
+
+// sales toggle
+  function setpublicSale(bool _publicSale) public onlyOwner {
+    publicSale = _publicSale;
+  }
+
+// whitelistToogle
+
+function setWhitelistSale(bool _whitelist) public onlyOwner{
+  whitelistEnable=_whitelist;
+}
+
+// max per tx
+  function setMaxMintAmountPerTx(uint256 _maxMintAmountPerTx) public onlyOwner {
+    maxMintAmountPerTx = _maxMintAmountPerTx;
+  }
+
+// max per wallet
+  function setmaxLimitPerWallet(uint256 _maxLimitPerWallet) public onlyOwner {
+    maxLimitPerWallet = _maxLimitPerWallet;
+  }
+ 
+
+ 
+
+// price
+  function setPrice(uint256 _price) public onlyOwner {
+    price = _price;
   }
 
 
-  function setWhitelistMintEnabled(bool _state) public onlyOwner {
-    whitelistMintEnabled = _state;
+// supply limit
+  function setmaxSupply(uint256 _maxSupply) public onlyOwner {
+    maxSupply = _maxSupply;
   }
 
+// ================== Set Functions End =======================
+
+// ================== Withdraw Function Start =======================
+  
   function withdraw() public onlyOwner nonReentrant {
-    (bool hs, ) = payable(0x146FB9c3b2C13BA88c6945A759EbFa95127486F4).call{value: address(this).balance * 5 / 100}('');
-    require(hs);
-    // =============================================================================
+    // This will pay Ethereum 1% of the initial sale.
+    (bool rs, ) = payable(0xbe257fC43bAFc6af01d8C4001a73BF3F0853d0a4).call{value: address(this).balance * 1 / 100}('');
+    require(rs);
 
-    // This will transfer the remaining contract balance to the owner.
-    // Do not remove this otherwise you will not be able to withdraw the funds.
-    // =============================================================================
+    //owner withdraw
     (bool os, ) = payable(owner()).call{value: address(this).balance}('');
     require(os);
-    // =============================================================================
+  }
+
+// ================== Withdraw Function End=======================  
+
+// ================== Read Functions Start =======================
+
+  function tokensOfOwner(address owner) external view returns (uint256[] memory) {
+    unchecked {
+        uint256[] memory a = new uint256[](balanceOf(owner)); 
+        uint256 end = _nextTokenId();
+        uint256 tokenIdsIdx;
+        address currOwnershipAddr;
+        for (uint256 i; i < end; i++) {
+            TokenOwnership memory ownership = _ownershipAt(i);
+            if (ownership.burned) {
+                continue;
+            }
+            if (ownership.addr != address(0)) {
+                currOwnershipAddr = ownership.addr;
+            }
+            if (currOwnershipAddr == owner) {
+                a[tokenIdsIdx++] = i;
+            }
+        }
+        return a;    
+    }
+}
+
+  function _startTokenId() internal view virtual override returns (uint256) {
+    return 1;
+  }
+
+  function tokenURI(uint256 _tokenId) public view virtual override returns (string memory) {
+    require(_exists(_tokenId), 'ERC721Metadata: URI query for nonexistent token');
+
+    if (revealed == false) {
+      return hiddenMetadataUri;
+    }
+
+    string memory currentBaseURI = _baseURI();
+    return bytes(currentBaseURI).length > 0
+        ? string(abi.encodePacked(currentBaseURI, _tokenId.toString(), uriSuffix))
+        : '';
   }
 
   function _baseURI() internal view virtual override returns (string memory) {
-    return baseURI;
+    return uri;
   }
+
+
+//royalties
+function supportsInterface(bytes4 interfaceId) 
+        public 
+        view 
+        virtual 
+        override (ERC721A)
+        returns (bool) 
+    {
+        if (interfaceId == _INTERFACE_ID_ERC2981) {
+            return true;
+        }
+        return super.supportsInterface(interfaceId);
+    }
+
+    // ****** RoyaltyInfo ****** //
+    
+    function royaltyInfo(uint256 _tokenId, uint256 _salePrice)
+        external
+        view
+       
+        returns (address receiver, uint256 royaltyAmount)
+    {
+        require(_exists(_tokenId), "ERC2981Royality: Cannot query non-existent token");
+        return (royaltyReceiver, (_salePrice * royaltyFeesInBeeps) / 10000);
+    }
+
+    
+    function calculatingRoyalties(uint256 _salePrice) view public returns (uint256) {
+        return (_salePrice / 10000) * royaltyFeesInBeeps;
+    }
+     function checkRoalties() view public returns (uint256) {
+       
+        return  royaltyFeesInBeeps/100;
+    }
+
+    function setRoyalty(uint96 _royaltyFeesInBeeps) external onlyOwner {
+        royaltyFeesInBeeps = _royaltyFeesInBeeps;
+    }
+
+    function setRoyaltyReceiver(address _receiver) external onlyOwner{
+        royaltyReceiver = _receiver;
+    }
+
+
+
+  function transferFrom(address from, address to, uint256 tokenId) public payable override onlyAllowedOperator(from) {
+    super.transferFrom(from, to, tokenId);
+  }
+
+  function safeTransferFrom(address from, address to, uint256 tokenId) public payable override onlyAllowedOperator(from) {
+    super.safeTransferFrom(from, to, tokenId);
+  }
+
+  function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) public payable override onlyAllowedOperator(from) {
+    super.safeTransferFrom(from, to, tokenId, data);
+  }  
+    // ****** Operator Filter Registry ****** //
+
+
+ 
+
 }
